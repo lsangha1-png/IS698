6c2e2ba25f8acd7343fbd187da50d22742e53c41
lsangha1-png <lsangha1@umbc.edu>
2025-11-03 16:24:59 -0500

    Update main.tf

--- Lab 7 /main.tf
+++ Lab 7 /main.tf
@@ -1,1 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-homework-lab7a"
+    key            = "terraform/state.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+	}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0bdd88bd06d16ba03"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "Terraform-Test-Instance"
+  }
+}
 
